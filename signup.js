5782f9b6df3c27008054830d9cb2c8ab3094526c
adityasinha198 <adityasinha198@gmail.com>
2022-09-20 19:27:41 +0530

    Task1

---
+++ signup.js
@@ -0,0 +1,19 @@
+function onSubmit(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const emailid=event.target.emailid.value;
+    const password=event.target.password.value;
+    console.log(name)
+    const obj = {
+        name,
+        emailid,
+        password
+    }
+   
+    axios.post("http://localhost:8000/signUp",obj)
+    .then(res=>{
+        console.log(res)
+    }).catch(err=>{
+        console.log(err)
+    })
+}
