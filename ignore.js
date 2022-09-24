031120906f5413e2460fe30bc6a55b3ddfb3472a
adityasinha198 <adityasinha198@gmail.com>
2022-09-24 23:45:58 +0530

    task9

--- ignore.js
+++
@@ -1,1 +0,0 @@
-console.log("Hell0o")
---
+++ practice .js
@@ -0,0 +1,18 @@
+obj = {
+    1000 : 1
+    ,500:3,
+    2000:
+    2
+
+}
+const arr = Object.keys(obj)
+//arr.sort()
+console.log(arr)
+
+for(let i =0;i<arr.length;i++){
+     console.log(`${arr[i]}:${obj[arr[i]]}`)
+
+
+ }
+ obj["Adi"] = 22
+ console.lo
